748a5d4273e9d7f26f62b81d6afae2b54b4e2795
hossein shoshtary <hossein.shoshtary46@gmail.com>
2025-11-12 22:53:43 +0330

    register is done

--- account/views.py
+++ account/views.py
@@ -1,8 +1,12 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 
 def user_login(request):
+    if request.user.is_authenticated == True:
+        return redirect("/")
+
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
@@ -11,3 +15,31 @@ def user_login(request):
             login(request, user)
             return redirect("/")
     return render(request, template_name="account/login.html", context={})
+
+
+def user_register(request):
+    context = {"errors": []}
+    if request.user.is_authenticated == True:
+        return redirect("/")
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password1 = request.POST.get("password1")
+        password2 = request.POST.get("password2")
+        if password1 != password2:
+            context['errors'].append("passwords dose not in same")
+            return render(request, template_name="account/register.html", context=context)
+
+        # if User.objects.get(username=username):
+        #     context['errors'].append("this username is exists")
+        #     return render(request, template_name="account/register.html", context=context)
+        user = User.objects.create(username=username, password=password1, email=email)
+        login(request, user)
+        return redirect("/")
+    return render(request, template_name="account/register.html", context={})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect("/")
